0934a48ffa053d2641059c98293c8800120abcae
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2021-07-25 13:32:39 -0500

    Chore: Add view [Post, Posts], Add Debugbar, add Pagination

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PageController extends Controller
+{
+    public function posts(){
+        return view('posts', [
+            'posts' => Post::with('user')->latest()->paginate(10)
+        ]);
+    }
+
+    public function post(Post $post){
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@posts');
+Route::get('blog/{post}','PageController@post')->name('post');
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
